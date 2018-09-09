6385bfbd11af1f066457d6df93fb5ed09a610144
Osmand Das <osmandpdas@gmail.com>
2018-09-08 20:49:40 -0400

    updated

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe} from '../recipe.model';
 
 @Component({
@@ -8,12 +8,19 @@ import { Recipe} from '../recipe.model';
 })
 export class RecipelistComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe','This is simply a test', 'https://www.eatwell101.com/wp-content/uploads/2017/03/baked-Garlic-Butter-Chicken-recipe.jpg')
+    new Recipe('Keto Chicken Enchilada','The other day I was thinking about what kind of Mexican style food I could make that would not only LOOK the part, but taste satisfying too. The idea hit me that if I melted cheese to JUST the right consistency I could roll it around some stuffing to make an Enchilada type of dish. Usually when it comes to traditional enchiladas I prefer cheese enchiladas with red sauce, but I thought that stuffing Cheese with Cheese might be overkill. (Feel free to knock yourself out though if you want to try that cheese inception)', 'https://keto-daily.com/wp-content/uploads/2018/07/Keto-Chicken-Enchilada-Recipe-Wide.jpg'),
+    new Recipe('Lidia Bastianich\'s Stuffed Peppers','Heat a dutch oven large enough to hold the peppers upright in one layer over medium heat. Add the olive oil. When the oil is hot, add the onion, and cook until softened, about 10 minutes. Spoon about  half the onion into a large bowl to cool. ','https://www.cbc.ca/food/content/images/recipes/StuffedPeppers1.png')
   ];
+
+  @Output() recipeSelectedFromList = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(selectedRecipe: Recipe){
+    this.recipeSelectedFromList.emit(selectedRecipe)
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -18,4 +18,8 @@ export class ShoppinglistComponent implements OnInit {
   ngOnInit() {
   }
 
+  onIngredientAdded(ingredient: Ingredient){
+    this.ingredients.push(ingredient);
+  }
+
 }
